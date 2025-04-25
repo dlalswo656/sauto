84c42b3eecc6148a2fbd0254e081c70e3c7b424f
MusicShare <dlalswo656@nate.com>
2025-04-25 23:30:58 +0900

    sauto 주식 자동 매매 프로그램

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    // 시작
+    public static void main(String[] args) {
+        System.out.println("주식 자동 매매 프로그램 프로젝트");
+    }
+}
